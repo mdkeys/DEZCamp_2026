befb0b2ef23a9c5a64930ce656d7f0fa4f766a97
Misha Davies <mishiya@gmail.com>
2026-02-15 05:14:12 +0000

    Update fct_trips

--- Week_4/models/marts/fct_trips.sql
+++ Week_4/models/marts/fct_trips.sql
@@ -1,4 +1,3 @@
--- I need to revisit this file. I lost the entire thing so this is just a copy of the DEZC example file. ARGH. 
 {{
   config(
     materialized='incremental',
@@ -60,26 +59,3 @@ left join {{ ref('dim_zones') }} as dz
   -- Only process new trips based on pickup datetime
   where trips.pickup_datetime > (select max(pickup_datetime) from {{ this }})
 {% endif %}
-
-/* Comment out old code 
-WITH trips_unioned AS (
-	SELECT * FROM {{ ref('int_trips_unioned') }}
-),
-
-cte_trip_id AS (
-	SELECT 
-        {{ get_trip_id('vendor_id','rate_code_id','pickup_location_id',
-                    'dropoff_location_id','pickup_datetime','dropoff_datetime',
-                    'store_and_fwd_flag','passenger_count','trip_type','trip_distance',
-                    'fare_amount','extra','mta_tax','tip_amount','tolls_amount',
-                    'improvement_surcharge','ehail_fee','total_amount','payment_type') 
-        }} as trip_id
-	FROM trips_unioned
-)
-
-SELECT *, count(*) as dup_n
-FROM cte_trip_id
-GROUP BY trip_id
-ORDER BY dup_n DESC 
-
-*/
